3d80f392b9f58e2350938c0c68adad7638ce2089
SWAYAMSIDDHAPANDA <swayampanda03@gmail.com>
2026-04-03 13:21:07 +0530

    Added 28_Find First Occurrence in a String solution

--- Easy/28_FindFirstOccurrence.java
+++ Easy/28_FindFirstOccurrence.java
@@ -1,4 +1,4 @@
-// Problem: 28. Find the Index of the First Occurrence in a String
+// Problem: 28_Find the Index of the First Occurrence in a String
 // Link: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 
 // Approach:
