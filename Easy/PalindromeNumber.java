d26a56caafaae78c02f91b2025367f1266a88fd0
SWAYAMSIDDHAPANDA <swayampanda03@gmail.com>
2026-04-03 12:44:33 +0530

    Added Palindrome Number solution
    Implement a solution to check if a number is a palindrome by reversing its digits.

---
+++ Easy/PalindromeNumber.java
@@ -0,0 +1,23 @@
+// Problem: Palindrome Number
+// Link: https://leetcode.com/problems/palindrome-number/
+
+// Approach:
+// We reverse the given number by extracting its digits one by one.
+// Then we compare the reversed number with the original number.
+// If both are equal, the number is a palindrome; otherwise, it is not.
+
+// Time Complexity: O(log₁₀(n))
+// Space Complexity: O(1)
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        int reverse = 0;
+        for (int i = x; i > 0; i = i / 10) {
+            reverse = reverse * 10 + (i % 10);
+        }
+        if (x == reverse) {
+            return true;
+        }
+        return false;
+    }
+}
