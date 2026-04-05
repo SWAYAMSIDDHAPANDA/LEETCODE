d0b46c03c1abd6025e3c86697ef1f12aafd12437
SWAYAMSIDDHAPANDA <swayampanda03@gmail.com>
2026-04-05 15:29:32 +0530

    Added 2235_Add Two Integers solution

---
+++ Easy/2235_AddTwoIntegers.java
@@ -0,0 +1,15 @@
+// Problem: 2235_Add Two Integers
+// Link: https://leetcode.com/problems/add-two-integers/
+
+// Approach:
+// Simply return the sum of the two given integers.
+
+// Time Complexity: O(1)
+
+// Space Complexity: O(1)
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return (num1 + num2);
+    }
+}
