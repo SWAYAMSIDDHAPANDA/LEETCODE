3ee28d155e3868c9ddd16849967afedd595e06d3
SWAYAMSIDDHAPANDA <swayampanda03@gmail.com>
2026-04-06 13:44:44 +0530

    Added 509_Fibonacci Number solution
    Implemented Fibonacci number calculation using recursion.

---
+++ Easy/509_FibonacciNumber.java
@@ -0,0 +1,20 @@
+// Problem: 509. Fibonacci Number
+// Link: https://leetcode.com/problems/fibonacci-number/
+
+// Approach:
+// Use recursion based on the definition:
+// fib(n) = fib(n-1) + fib(n-2)
+// Base cases: fib(0) = 0, fib(1) = 1
+
+// Time Complexity: O(2^n)
+
+// Space Complexity: O(n)
+
+class Solution {
+    public int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+}
